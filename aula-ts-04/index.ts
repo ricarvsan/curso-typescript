6eea90aee02abc93159390639d953f9682903379
Ricardo Carvalho dos Santos <ricarvsan@gmail.com>
2023-09-18 10:58:46 -0300

    exercicios 4 ao 6

--- aula-ts-04/index.ts
+++ aula-ts-04/index.ts
@@ -1,20 +1,20 @@
-function multiply(num1: number, num2: number) {
+function multiply(num1:number , num2:number):number {
     return num1 * num2;
 }
 
-function sum(num1: number, num2: number) {
-    return num1 * num2;
+function sum(num1:number , num2:number):number {
+    return num1 + num2;
 }
 
-function isEven(num: number) {
+function isEven(num:number):boolean {
     return num % 2 === 0;
 }
 
-function showResult(result: number) {
+function showResult(result:number):void {
     if(isEven(result)) {
         console.log(`The result is ${result} and it's even!`);
     } else {
-        console.log(`The result is ${result} and it's even!`);
+        console.log(`The result is ${result} and it's odd!`);
     }
 }
 
@@ -22,12 +22,12 @@ function showResult(result: number) {
     const num1 = Number(prompt("First Number"));
     const num2 = Number(prompt("Second Number"));
 
-    if(!isNaN(num1) && !isNaN(num2)) {
-        let result = sum(num1,num2);
-        result += multiply(1,2);
-        showResult(result);
-    } else {
-        console.log("Não é possível executar com estes valores.")
+    if(isNaN(num1) || isNaN(num2)) {
+        console.log('Digite apenas números.');
+        return 0;
     }
-
+    
+    let result = sum(num1,num2);
+    result += multiply(1,2);
+    showResult(result);
 })();
--- aula-ts-05/index.ts
+++ aula-ts-05/index.ts
@@ -1,33 +1,5 @@
-function multiply(num1: number, num2: number) {
-    return num1 * num2;
-}
-
-function sum(num1: number, num2: number) {
-    return num1 * num2;
-}
-
-function isEven(num: number) {
-    return num % 2 === 0;
-}
-
-function showResult(result: number) {
-    if(isEven(result)) {
-        console.log(`The result is ${result} and it's even!`);
-    } else {
-        console.log(`The result is ${result} and it's even!`);
-    }
-}
-
-(() => {
-    const num1 = Number(prompt("First Number"));
-    const num2 = Number(prompt("Second Number"));
-
-    if(!isNaN(num1) && !isNaN(num2)) {
-        let result = sum(num1,num2);
-        result += multiply(1,2);
-        showResult(result);
-    } else {
-        console.log("Não é possível executar com estes valores.")
-    }
-
-})();
+const numbers:number[] = [1,2,3,4,5];
+const anwsers:boolean[] = [true, false, false, true, false];
+const strings:string[] = ["one", "two", "three", "four", "five"];
+const mix:(number|string)[] = [1,"two",3,"four",5];
+const floatNumbers:number[] = [1.0,2.5,3.0,4.1,5.5];
